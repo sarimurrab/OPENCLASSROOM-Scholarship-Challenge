a658307e6cf56275492692af58b0e648fb3cc4fc
Chaudhary Sarimurrab <35756749+sarimurrab@users.noreply.github.com>
2020-01-29 12:01:47 +0530

    delete

--- Crs2 Learn Programming With JavaScript/1 Variables.js
+++
@@ -1,9 +0,0 @@
-// Create your variables here
-// ==========================================
-let numberOfSeasons = 6;
-let numberOfEpisodes = 12;
-
-
-// ==========================================
-
-export { numberOfSeasons, numberOfEpisodes };
